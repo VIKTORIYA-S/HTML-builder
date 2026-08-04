ee183119dbc735550eb3d96ae11cddd6ae1e3b8e
victoria <v.suprun1961@gmail.com>
2026-08-04 08:18:22 +0300

    feat: add task 05-merge-styles (вторник, августа 04, 2026 08:18:22)

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const stylesDir = path.join(__dirname, 'styles');
+const distDir = path.join(__dirname, 'project-dist');
+
+const bundlePath = path.join(distDir, 'bundle.css');
+
+fs.mkdir(distDir, { recursive: true }, (err) => {
+    console.log('готово');
+    fs.rm(bundlePath, { force: true }, (err) => {
+      const writeStream = fs.createWriteStream(bundlePath, { flag: 'a' });
+      fs.readdir(stylesDir, (err, files) => {
+        files.forEach((file) => {
+          if (path.extname(file) === '.css') {
+            fs.createReadStream(path.join(stylesDir, file)).pipe(writeStream);
+          }
+        });
+      });
+    });
+});
